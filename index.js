497e8ff4901e24bc55f6407b8874336a0c63b93c
Oleg V. Anisov <anisov@obninsk.ru>
2020-05-28 18:01:52 +0300

    add timeout

--- index.js
+++ index.js
@@ -5,13 +5,21 @@ const port = process.env.PORT || 3000;
 const interval = process.env.INTERVAL || 1000;
 const duration = process.env.DURATION || 30000;
 
-console.log(interval, duration);
-
 const httpServer = http.createServer((req, res) => {
-  res.write('Hello World!');
-  res.end();
+  const intervalId = setInterval(() => {
+    console.log('setInterval');
+  }, interval);
+
+  setTimeout(() => {
+    clearInterval(intervalId);
+    console.log('end');
+    res.end('Hello Client!');
+  }, duration);
 });
 
-httpServer.listen(port, () => {
+httpServer.listen(port, (err) => {
+  if (err) {
+    return console.log('Something went wrong!');
+  }
   console.log(`Server running on port: ${port}`);
 });
