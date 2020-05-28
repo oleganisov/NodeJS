cfb785a17f152dda8fba4e1c8c3539cdd02f35c7
Oleg V. Anisov <anisov@obninsk.ru>
2020-05-28 16:50:31 +0300

    add http server

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 console.log('index');
+const http = require('http');
+
+const port = process.env.PORT || 3000;
+const interval = process.env.INTERVAL || 1000;
+const duration = process.env.DURATION || 30000;
+
+console.log(interval, duration);
+
+const httpServer = http.createServer((req, res) => {
+  res.write('Hello World!');
+  res.end();
+});
+
+httpServer.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
